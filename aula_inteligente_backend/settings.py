98a236a662627c5544d985fe54d30cd05e381e23
Alex Orellana <lexorel88@gmail.com>
2025-06-05 05:58:30 -0400

    ok

--- aula_inteligente_backend/settings.py
+++ aula_inteligente_backend/settings.py
@@ -78,7 +78,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'aula_inteligente_backend.wsgi.application'
 
 
-# Database
+# Database #########
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 import os
